4b45efa2b7a69e65a1517b3c614bc9a25b1c48f6
ElizaValle <elizabetevalle7@gmail.com>
2023-02-16 14:16:35 -0300

    layout pronto

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Jogo from "./components/Jogo"
+import Letras from "./components/Letras"
+
+export default function App() {
+
+  return (
+    <div>
+      <Jogo />
+      <Letras />
+    </div>
+  );
+}
+
